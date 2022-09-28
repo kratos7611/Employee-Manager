f2be1f9951e58e9903a1cebf38f3502b8d54adac
nischal <nischal.b.adhikari@gmail.com>
2022-09-28 19:35:28 +0545

    Made User Dashboard

--- src/Pages/Dashboard/Dashboard.jsx
+++ src/Pages/Dashboard/Dashboard.jsx
@@ -1,13 +1,18 @@
-import { Stack } from '@mui/system'
-import React from 'react'
-import Sidebar from '../../components/Sidebar/Sidebar'
+import { Stack, Box} from "@mui/system";
+import { Grid } from '@mui/material';
+import React from "react";
+import Sidebar from "../../components/Sidebar/Sidebar";
+import Feed from "../../components/Feed/Feed";
 
 const Dashboard = () => {
   return (
-    <Stack>
+    <Stack direction="row" spacing={2}>
+      <Grid container>
         <Sidebar />
+        <Feed />
+      </Grid>
     </Stack>
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;
